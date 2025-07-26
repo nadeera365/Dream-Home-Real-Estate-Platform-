83cb4750ad0326a22cd882018db8179e3fd8abc8
Nadeera Kumara <nadeerakumara140@gmail.com>
2025-07-26 22:11:33 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="register.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <title>Luxury Real Estate - Login</title>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar">
+          <div class="nav-container">
+            
+            <div class="logo-area">
+              <a href="home.php"><img     src="resources/logo-design-concept-real-estate-business.png" alt="Real     Estate Logo" class="logo" /></a>
+              
+            </div>
+    
+            <ul class="nav-links">
+              <li><a href="home.php">Home</a></li>
+              <li><a href="properties.php">Properties</a></li>
+              <li><a href="aboutus.php">About </a></li> 
+              <li><a href="contactus.php">Contact</a></li>
+            </ul>
+            
+          </div>
+        
+        </nav>
+    </header> 
+
+    <div class="banner">
+        <img class="banner-img" src="resources/luxury-architecture-exterior-design.jpg" alt="Luxury Real Estate Banner">
+        <div class="login-container">
+            <h2>Create your account</h2>
+
+            <form action="registerinput.php" method="post" class="login-form">
+
+              <select name="role" id="role" required>
+                <option value="">-- Choose Role --</option>
+                <option value="user">User</option>
+                
+              </select>
+               
+               <label for="f_name">First name</label>
+               <input type="text" name="f_name" id="f_name" placeholder="Enter your first name" required>
+
+               <label for="l_name">Last name</label>
+               <input type="text" name="l_name" id="l_name" placeholder="Enter your last name" required>
+           
+               <label for="email">Email</label>
+               <input type="email" name="email" id="email" placeholder="Enter your email" required>
+           
+               <label for="password">Password</label>
+               <input type="password" name="pass" id="password" placeholder="Enter your password" required>
+
+               <label for="confirmpassword">Confirm Password</label>
+               <input type="password" name="confirm_pass" id="password" placeholder="Enter your password" required>
+           
+               <button type="submit" name="submit">Register</button>
+               
+           
+               <p class="register-link">Already have an account?<a href="login.php">Login here</a></p> 
+            </form>
+        </div> 
+    </div>
+
+
+    <div class="footer-container">
+        <footer class="footer">
+          
+          <!-- Top Social Bar -->
+          <div class="footer-social">
+            <span>Connect with us:</span>
+            <div class="social-icons">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-google"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-linkedin"></i></a>
+              <a href="#"><i class="fab fa-github"></i></a>
+            </div>
+          </div>
+
+          <div class="footer-content">
+            <div class="footer-box">
+              <h4>Company</h4>
+              <p>We help you find your dream property with ease, trust, and professionalism.</p>
+              <img class="logo-footer" src="resources/logo-design-concept-real-estate-business.png" alt="">
+            </div>
+      
+            <!-- Services -->
+            <div class="footer-box">
+              <ul>
+                <li><a href="properties.php">View Property</a></li>
+                <li><a href="properties.php">View Property</a></li>
+                <li><a href="aboutus.html">About us</a></li>
+                <li><a href="contactus.html">Agent Support</a></li>
+              </ul>
+            </div>
+      
+            <!-- Links -->
+            <div class="footer-box">
+              <h4>Quick Links</h4>
+              <ul>
+                <li><a href="#">Your Account</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+      
+            <!-- Contact Info -->
+            <div class="footer-box">
+              <h4>Contact</h4>
+              <p><i class="fas fa-map-marker-alt"></i> kegalle road, Sabaragamuwa Province, Sri Lanka</p>
+              <p><i class="fas fa-envelope"></i> info@dreamhomes.com</p>
+              <p><i class="fas fa-phone"></i> +94 773691188</p>
+            </div>
+      
+          </div>
+      
+          <!-- Copyright -->
+          <div class="footer-bottom">
+            <p>&copy; 2025 Dream Homes. All rights reserved.</p>
+          </div>
+      
+        </footer>
+    </div>
+    
+
+
+</body>
+</html>
